d6afeb1f9664e36afa98dc048356d0a6bb702c2e
karinfr <karinf@zillowgroup.com>
2021-03-02 14:40:54 -0800

    feat: typing - HEIC

--- index.d.ts
+++ index.d.ts
@@ -8,7 +8,7 @@ declare module 'react-native-image-resizer' {
     height: number;
   }
 
-  export type ResizeFormat = 'PNG' | 'JPEG' | 'WEBP';
+  export type ResizeFormat = 'PNG' | 'JPEG' | 'WEBP' | 'HEIC';
   export type ResizeMode = 'contain' | 'cover' | 'stretch';
 
   export default class ImageResizer {
@@ -22,25 +22,25 @@ declare module 'react-native-image-resizer' {
       outputPath?: string,
       keepMeta?: boolean,
       options?: {
-        /** 
-         * Either `contain` (the default), `cover`, or `stretch`. Similar to 
+        /**
+         * Either `contain` (the default), `cover`, or `stretch`. Similar to
          * [react-native <Image>'s resizeMode](https://reactnative.dev/docs/image#resizemode)
-         * 
+         *
          * - `contain` will fit the image within `width` and `height`,
          *   preserving its ratio
          * - `cover` will make sure at least one dimension fits `width` or
          *   `height`, and the other is larger, also preserving its ratio.
          * - `stretch` will resize the image to exactly `width` and `height`.
-         *   
+         *
          * (Default: 'contain')
          */
         mode?: ResizeMode;
-        /** 
+        /**
          * Whether to avoid resizing the image to be larger than the original.
          * (Default: false)
          */
         onlyScaleDown?: boolean;
-      },
+      }
     ): Promise<Response>;
   }
 }
